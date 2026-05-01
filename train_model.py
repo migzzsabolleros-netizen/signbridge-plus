81e03401db10253581b3cc0aa60be5595d130b39
migzzsabolleros-netizen <migzzsabolleros@gmail.com>
2026-05-01 18:30:16 +0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -8,13 +8,8 @@ from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
 import matplotlib.pyplot as plt
 
 # ---- SETTINGS (must match collect_data.py) ----
-<<<<<<< HEAD
-SIGNS = ['pamilya','lola','lolo','mama','papa']
-SEQUENCES = 60
-=======
-SIGNS = ['good_morning', 'good_afternoon', 'good_evening']
+SIGNS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q']
 SEQUENCES = 30
->>>>>>> 2c383ee2c00e73545b0df12cd499f5186665dc52
 SEQUENCE_LENGTH = 30
 DATA_PATH = 'dataset'
 
